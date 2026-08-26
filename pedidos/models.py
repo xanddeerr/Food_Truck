2f668940fb6d299c4e70eea7388be1c85d6cdea7
Alumnos Copiapó Laboratorio 405L <COP405L@inacap.cl>
2026-08-25 22:32:54 -0400

    Segundo commit

--- pedidos/models.py
+++ pedidos/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+class Categoria (models.Model):
+    nombre = models.CharField(max_length=50)
+
+    class Meta:
+        verbose_name = "Categoría"
+        verbose_name_plural = "Categorías"
+        ordering = ["nombre"]
+
+    def __str__(self):
+        return self.nombre
+
+class Producto (models.Model):
+    nombre = models.CharField(max_length=100)
+    precio = models.IntegerField()
+
+    class Meta:
+        verbose_name = "Producto"
+        verbose_name_plural = "Productos"
+        ordering = ["nombre"]
+
+    def __str__(self):
+        return self.nombre
+    
